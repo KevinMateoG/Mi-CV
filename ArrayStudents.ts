1d89b1ec0586587afac69fdfc4d13e869be8a776
Kevin Mateo Gomez <gomezdiezkevinmateo@gmail.com>
2026-08-21 17:46:05 -0500

    Add console logs for section headers in ArrayStudents.ts

--- ArrayStudents.ts
+++ ArrayStudents.ts
@@ -1,8 +1,12 @@
 // Punto 1
+console.log('Punto 1\n');
+
 const arrayStudent: String[] = ['kevin', 'sebastian', 'tafur', 'heiber', 'miguel']
 
 for (let index = 0; index < arrayStudent.length; index++) {
     console.log(arrayStudent[index]);
 }
 // Punto 2
+console.log('Punto 2\n');
+
 console.log('en este array contiene '+ arrayStudent.length + ' estudiantes');
